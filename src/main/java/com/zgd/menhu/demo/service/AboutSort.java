4c10f8c0ac775591bdced21daaf49cfb424c2b76
zgd <zgd@zgddeMBP.lan>
2022-07-23 15:50:52 +0800

    排序算法

--- src/main/java/com/zgd/menhu/demo/service/AboutSort.java
+++ src/main/java/com/zgd/menhu/demo/service/AboutSort.java
@@ -1,13 +1,63 @@
 package com.zgd.menhu.demo.service;
 
+import java.util.Random;
+
 public class AboutSort {
 
     public static void main(String[] args) {
         AboutSort sort = new AboutSort();
-        int arr[] = {-2,3,-5};
-        sort.heapSort(arr);
+        int arr[] = {5,1,2,0,0};
+        //sort.heapSort(arr);
+        sort.qucikSort(arr, 0, arr.length  -1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + "->");
+        }
     }
 
+
+
+    public void qucikSort(int[] nums, int left, int right) {
+        if (left < right) {
+            int partion = partition(nums, left, right);
+            qucikSort(nums, left, partion - 1);
+            qucikSort(nums, partion + 1, right);
+        }
+    }
+
+    private int partition(int [] arr, int left, int right) {
+        int posrandom = new Random().nextInt(right - left +1) + left;
+        swap(arr, posrandom, left);
+        int pos = left;
+        int temp = arr[left];
+        while (left < right) {
+
+            while (arr[right] >= temp && left < right) {
+                right --;
+            }
+
+            while (arr[left] <= temp && left < right) {
+                left ++;
+            }
+
+            if (right > left) {
+                swap(arr, left, right);
+            }
+        }
+        arr[pos] = arr[left];
+        arr[left] = temp;
+        return left;
+    }
+
+
+
+
+
+
+
+    /**
+     * 堆排序
+     * @param arr
+     */
     public void heapSort(int arr[]) {
         //建堆
         for (int i = (arr.length - 1)/2; i >=0; i -- ) {
@@ -40,6 +90,7 @@ public class AboutSort {
      */
     public void ajustHeap(int arr[], int k, int length) {
 
+        //
         for (int i = 2*k + 1; i < length; i = 2*i + 1) {
             if (i + 1 <= length -1 && arr[i] < arr[i +1]) {
                 i = i +1;
@@ -51,5 +102,14 @@ public class AboutSort {
             }
             k = i;
         }
+    }
+
+
+    /**
+     * 归并排序
+     * @param arr
+     */
+    public void mergeSort(int arr[]) {
+
     }
 }
